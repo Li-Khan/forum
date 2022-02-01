a2e38e607368b64032159ac7984131d56aff7b48
Li-Khan <alihanzh40@gmail.com>
2022-02-01 12:32:13 +0600

    added: url request processing

--- web/handlers.go
+++ web/handlers.go
@@ -3,6 +3,7 @@ package web
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func (app *Application) home(w http.ResponseWriter, r *http.Request) {
@@ -21,25 +22,71 @@ func (app *Application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Application) signin(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "signin page")
+	switch r.Method {
+	case http.MethodGet:
+		// Обработка страницы
+		fmt.Fprint(w, "signin page")
+	case http.MethodPost:
+		// Обработка запроса
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
 }
 
 func (app *Application) signup(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "signup page")
+	switch r.Method {
+	case http.MethodGet:
+		// Обработка страницы
+		fmt.Fprint(w, "signup page")
+	case http.MethodPost:
+		// Обработка запроса
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
 }
 
 func (app *Application) signout(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "signout page")
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	// Обработка выхода
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func (app *Application) profile(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "profile page")
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "profile id: %v", id)
 }
 
 func (app *Application) createPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "create post page")
+	switch r.Method {
+	case http.MethodGet:
+		// Обработка страницы
+		fmt.Fprint(w, "create post page")
+	case http.MethodPost:
+		// Обработка запроса
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
 }
 
 func (app *Application) createComment(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "create comment page")
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	// Обработка запроса
 }
